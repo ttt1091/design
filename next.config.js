530fa245425ad453329cbe390038f51fd083c3c7
ttt1091 <tez1091@yahoo.co.jp>
2021-11-18 10:23:50 +0900

    change

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  experimental: {
+    optimizeFonts: true,
+  },
   reactStrictMode: true,
 }
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -21,6 +21,9 @@ const Layout:NextPage<Props> = ({ children, home }: Props) => {
     <>
       <Head>
         <meta name="format-detection" content="telephone=no" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Sawarabi+Mincho&family=Yuji+Syuku&display=swap" rel="stylesheet" />
         <title>YoYos</title>
       </Head>
       <HeaderBar />
